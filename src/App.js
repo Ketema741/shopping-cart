d3f8cde6f9b91a86042f2f14105c3699976cf86e
KetemagitLab <kgirma363@gmail.com>
2022-05-18 20:37:11 +0300

    adding product components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import './App.css';
+import React from "react";
+import data from "./data.json"
+import Products from "./components/Products";
+// import Filter from "./components/Filter";
+// import Cart from "./components/Cart";
+// import store from "./store";
+// import { Provider } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      products:data.products,
+      size:"",
+      sort:"",
+    }
+  }
+  render() {
+    return (
+      // <Provider store={store}>
+        <div className="grid-container">
+          <header>
+            <a href="/">React Shopping Cart</a>
+          </header>
+          <main>
+            <div className="content">
+              <div className="main">
+                {
+                /* <Filter></Filter>*/
+                <Products products={this.state.products}></Products> 
+                }
+              </div>
+              <div className="sidebar">
+                {/* <Cart /> */}
+              </div>
+            </div>
+          </main>
+          <footer>All right is reserved.</footer>
+        </div>
+      // </Provider>
+    );
+  }
 }
 
 export default App;
